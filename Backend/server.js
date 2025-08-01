e8f668cbd353b03d032880058f72c387c3ab6599
JenilK04 <kovadiyajenil11@gmail.com>
2025-08-01 18:18:41 +0530

    admin manage users

--- Backend/server.js
+++ Backend/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 require('dotenv').config();
 const authRoutes = require('./routes/authroutes');
 const propertiesRoute = require('./routes/propertiesRoute');
-
+const adminroutes = require('./routes/adminroutes.js');
 const app = express();
 app.use(cors());
 
@@ -24,6 +24,7 @@ mongoose.connect(process.env.MONGO_URI, {
 app.get('/', (req, res) => res.send('API Running'));
 app.use('/api/auth', authRoutes);
 app.use('/api/properties', propertiesRoute);
+app.use('/api/admin',adminroutes)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
