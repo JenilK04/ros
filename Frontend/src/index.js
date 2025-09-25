9c31930f59b31e84ad46ff69534939fa9c695413
JenilK04 <kovadiyajenil11@gmail.com>
2025-09-25 17:10:05 +0530

    anaylisys

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -3,17 +3,28 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { PostHogProvider } from 'posthog-js/react';
 // import { AuthProvider } from './Context/authContext'; 
 import { UserProvider } from './Context/userContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <AuthProvider> */}
-      <UserProvider>   
-      <App />
-      </UserProvider>
-    {/* </AuthProvider> */}
+    <PostHogProvider
+      apiKey={process.env.REACT_APP_POSTHOG_KEY}
+      options={{
+        api_host: process.env.REACT_APP_POSTHOG_HOST,
+        defaults: '2025-05-24',
+        capture_exceptions: true, // This enables capturing exceptions using Error Tracking, set to false if you don't want this
+        debug: process.env.NODE_ENV === "development",
+      }}
+    >
+      {/* <AuthProvider> */}
+        <UserProvider>   
+          <App />
+        </UserProvider>
+      {/* </AuthProvider> */}
+    </PostHogProvider>
   </React.StrictMode>
 );
 
